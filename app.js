f84f2c6e3a44b6b3f68ab4f8cf83760a17ba9665
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-09-03 20:42:31 -0400

    l21

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// lesson 20
+// lesson 21
     
-import React from 'react';
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContext';

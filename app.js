92b0259668bd812f8e8f48e633632497f5bae800
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-09-03 20:40:04 -0400

    l17

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-// lesson 16
+// lesson 17
     
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContext';
+import BookList from './components/BookList';
+
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
